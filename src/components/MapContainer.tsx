b7a24afad58c7942b6d3e0c36a6b421152c930cf
Gabriel Wu <qqbbnease1004@126.com>
2019-01-13 10:12:54 +0800

    feat: full screen map

---
+++ src/components/MapContainer.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import {
+  GoogleAPI,
+  GoogleApiWrapper,
+  InfoWindow,
+  Marker,
+  Map,
+} from 'google-maps-react'
+
+interface MapContainerProps {
+  google: GoogleAPI
+}
+
+const MapContainer: React.FunctionComponent<MapContainerProps> = ({google}) => {
+  const [selectedPlace, selectPlace] = useState({name: ''})
+
+  return (
+    <Map
+      google={google}
+      zoom={14}
+      initialCenter={{
+        lat: 42.39,
+        lng: -72.52,
+      }}
+    >
+      {/* <Marker />
+
+      <InfoWindow google={google} map={} marker={}>
+        <div>
+          <h1>{selectedPlace.name}</h1>
+        </div>
+      </InfoWindow> */}
+    </Map>
+  )
+}
+
+export default GoogleApiWrapper(props => ({
+  apiKey: 'AIzaSyC6I-uL4lzPx0CzyOzyYSdnibxVrsfVy6g',
+  language: props.language,
+}))(MapContainer)
